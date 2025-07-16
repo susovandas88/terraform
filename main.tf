b37c6fc15929a6971190ae40d8f09cc7986cb526
susovandas88 <susovandas88@gmail.com>
2025-07-16 23:05:31 +0530

    Testing Commit

--- main.tf
+++ main.tf
@@ -37,9 +37,9 @@ resource "aws_instance" "main" {
   security_groups = [aws_security_group.main.name]
 }
 
-output "instance_public_ip" {
-  value = aws_instance.main.public_ip
-}
+# output "instance_public_ip" {
+#   value = aws_instance.main.public_ip
+# }
 
 # output "vpc_id" {
 #   value = aws_vpc.main.id
