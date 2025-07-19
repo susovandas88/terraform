3c86f53103e298fd9bb70c89b331853db73c0a66
susovandas88 <susovandas88@gmail.com>
2025-07-19 12:17:52 +0530

    replacing sensitive data

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.availability_zone #"us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    region = var.availability_zone
+    bucket = var.s3_bucket_name
+    key    = var.s3_bucket_key
+  }
 }
